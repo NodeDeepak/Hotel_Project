9a7ad15e95923dbc0a8056ae71791c1b5dae9923
Deepak Govinda <itechno.deepakqa@gmail.com>
2023-05-17 22:59:50 +0530

    create user schema

--- resources/users/user.module.js
+++ resources/users/user.module.js
@@ -1,13 +1,41 @@
+const { boolean } = require("joi");
 const mongoose = require("mongoose");
 
+// let userSchema = new mongoose.Schema({
+//     profilePhoto: '',
+//     name: {type: String, default: ''},
+//     email: {type: String, default: ''},
+//     password: {type: String, default: ''},
+//     mobileNo: {type: Number, default: ''},
+//     address: {type: String, default: ''},
+//     token: {type: String, default: ''}
+// }, {timestamps: true});
+
+
 let userSchema = new mongoose.Schema({
-    profilePhoto: '',
-    name: {type: String, default: ''},
-    email: {type: String, default: ''},
-    password: {type: String, default: ''},
-    mobileNo: {type: Number, default: ''},
-    address: {type: String, default: ''},
-    token: {type: String, default: ''}
+    userInfo:{
+        profilePhoto: {type: String, default: ''},
+        name: {type: String, default: ''},
+        email: {type: String, default: ''},
+        password: {type: String, default: ''},
+        mobileNo: {type: Number, default: ''},
+        address: {type: String, default: ''},
+    },
+    businessInfo:{
+        businessProfilePhoto: {type: String, default: ''},
+        businessName: {type: String, default: ''},
+        email: {type: String, default: ''},
+        password: {type: String, default: ''},
+        mobileNo: {type: Number, default: ''},
+        address: {type: String, default: ''},
+    },
+    tokens:{
+        authToken : {type: String, default: ''},
+        fcmToken : {type: String, default: ''},  /// used for notification purpose 
+        deviceId : {type: Number, default: ''}   // 1=> ios , 2=> android
+    },
+    isVerified : {type: Boolean, default: false},
+    accountStatus: {type: String, enum:["Active", "Inactive"], default: 'Active'}
 }, {timestamps: true});
 
 
